7c9541cbda889fbb9ee77d09f83b19911ea7a4d8
Filipe Câncio <filipe.cancio@outlook.com>
2019-11-16 20:13:32 -0300

    configure database

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import Routes from './routes';
+import db from './firebase'
 
-const App = () =><Routes />
+const document = db.collection('health-plus').doc('health-plus');
+
+const Ada = document.get().then((doc) => {
+      console.log(doc.id, '=>', doc.data());
+    })
+  .catch((err) => {
+    console.log('Error getting documents', err);
+  });
+
+function App() {
+        
+    return (<div>
+        <Routes />
+    </div>);
+}
 
 export default App;
