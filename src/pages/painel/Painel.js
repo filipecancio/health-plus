5844d977dba2ad3738396181a59c5999e397e33a
Cancio <filipecancio.qti@gmail.com>
2019-11-19 11:15:20 -0300

    update project

--- src/pages/painel/Painel.js
+++ src/pages/painel/Painel.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import './painel.css';
+import { Drawer, List, ListItem, ListItemText, ListItemIcon, AppBar, Toolbar, CssBaseline } from '@material-ui/core';
 
-const Painel = () =><div ><h1>painel</h1></div>
+const Painel = () =><div >
+    <CssBaseline/>
+    <AppBar position="fixed">
+        <Toolbar>
+            Health-Plus
+        </Toolbar>
+    </AppBar>
+    <Drawer variant="permanent">
+        <List>
+            {['Pacientes','Exames','Laudos'].map((text,index)=>(
+                <ListItem button key={text}>
+                    <ListItemText primary={text}/>
+                </ListItem>
+            ))}
+        </List>
+        
+    </Drawer>
+</div>
 
 export default Painel;
