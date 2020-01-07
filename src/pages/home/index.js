99acd44666958f57dfef22eb2c0ee819a4c20c35
Filipe Câncio <filipe.cancio@gmail.com>
2020-01-07 01:47:30 -0300

    reestruturação do css

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import Background from '../../assets/jpg/doctor-wallpaper.jpg';
+import Logo from "../../assets/svg/logo_01.svg";
 
-const wallpaper = {
+const background = {
     backgroundImage: `url(${Background})`,
     backgroundRepeat: 'no-repeat',
     backgroundPosition: 'center',
     backgroundSize: 'cover',
     width: '100%',
-    height: '100%'
+    height: '100%',
+    display: 'flex',
+    flexFlow: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center'
 };
 
 const Home = () =>(
-    <div style={wallpaper}>
-            <h3>Acesse aos laudos de exames da sua clínica de forma rápida e acessível.</h3>
+    <div style={background}>
+        <h1>Acesse aos laudos de exames da sua clínica<br/> de forma rápida e acessível.</h1>
+        <img src={Logo} alt="Logo da compainha Health-Plus" />
     </div>
 );
 
